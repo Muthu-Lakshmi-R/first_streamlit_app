ab8637cac3e1c8a684b1e97930e034dc4749d6e9
Muthu-Lakshmi-R <155086081+Muthu-Lakshmi-R@users.noreply.github.com>
2023-12-28 23:33:25 +0530

    Update streamlit_app.py
    added the food menu list

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Brakefast Menu')
+streamlit.text('Idly-$80 2pcs')
+streamlit.text('Dosa-$70')
+streamlit.text('pongal-$50')
